be290b60200326828dfb9bc9f272138d13c7b865
Tijl B <tijl.bormans@gmail.com>
2025-01-19 13:05:08 +0100

    derigster preset changed listener when stopping self

--- OpenGPSLogger/app/src/main/java/eu/tijlb/opengpslogger/LocationNotificationService.kt
+++ OpenGPSLogger/app/src/main/java/eu/tijlb/opengpslogger/LocationNotificationService.kt
@@ -84,6 +84,7 @@ class LocationNotificationService : Service() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         if (intent?.action == STOP_SERVICE) {
             Log.d("ogl-locationnotificationservice", "Stopping location polling from delete intent")
+            settingsHelper.deregisterPresetChangedListener(presetChangedListener)
             stopLocationUpdates()
             stopSelf()
             return START_NOT_STICKY
